a4900b34bffe28a080ca2b5ea2217f96b164347d
shigamete <daiki_02@ymail.ne.jp>
2022-05-23 17:47:01 +0900

    デプロイ時のトップ画面設定

--- db/migrate/20220521135903_create_items.rb
+++ db/migrate/20220521135903_create_items.rb
@@ -1,8 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      
-      t.timestamps
+      t.string  :name,     null: false
+      # t.text    :explan,   null: false
+      # t.integer :category, null: false
+      # t.integer :status,   null: false
+      # t.integer :shippingfee_payer, null: false
+      # t.integer :prefecture,    null: false
+      # t.integer :shipping_days, null: false
+      # t.integer :price,    null: false
+      # t.references :user,  null: false, foreign_key: true
+      # t.timestamps
     end
   end
 end
