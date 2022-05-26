6b4c7ceed66d73c7a279c69af1fea8f60b2ecca9
shigamete <daiki_02@ymail.ne.jp>
2022-05-26 16:32:10 +0900

    password英数字混合バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :password, presence: true, format:{with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]/, message: "は英数字混合で入力"}
   validates :lastname_full, presence: true
   validates :firstname_full, presence: true
   validates :lastname_kana, presence: true
