f9a2c43e545331b25e6407a5b6e6b0e1bc2d4e95
shigamete <daiki_02@ymail.ne.jp>
2022-05-27 23:01:48 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, presence: true, format:{with: /(?=.[a-zA-Z])(?=.\d)[a-zA-Z\d]/, message: "は英数字混合で入力"}
-  validates :lastname_full, presence: true
-  validates :firstname_full, presence: true
-  validates :lastname_kana, presence: true
-  validates :firstname_kana, presence: true
+  validates :lastname_full, presence: true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message: "は全角ひらがな、カタカナ、漢字で入力"}
+  validates :firstname_full, presence: true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message: "は全角ひらがな、カタカナ、漢字で入力"}
+  validates :lastname_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/,message: "はカタカナで入力"}
+  validates :firstname_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/,message: "はカタカナで入力"}
 
   has_many :items
 end
