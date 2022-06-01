b42325ae36e7ef81c98c50768a58f2b64edfe3c9
shigamete <daiki_02@ymail.ne.jp>
2022-06-01 15:42:56 +0900

    shippingfee_payer_id作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :shippingfee_payer
 
   validates :name, :explain, presence: true
 
-  validates :category_id, :status_id, numericality: { other_than: 1 , message: "が未選択です"}
+  validates :category_id, :status_id, :shippingfee_payer_id, numericality: { other_than: 1 , message: "が未選択です"}
 end
