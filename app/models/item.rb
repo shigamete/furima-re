60d915a5cdf35efac952feb5c5b4f8d8618d4a16
shigamete <daiki_02@ymail.ne.jp>
2022-06-01 15:49:04 +0900

    prefecture_id作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,9 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :status
   belongs_to :shippingfee_payer
+  belongs_to :prefecture
 
   validates :name, :explain, presence: true
 
-  validates :category_id, :status_id, :shippingfee_payer_id, numericality: { other_than: 1 , message: "が未選択です"}
+  validates :category_id, :status_id, :shippingfee_payer_id, :prefecture_id, numericality: { other_than: 1 , message: "が未選択です"}
 end
